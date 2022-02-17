a907b247bc033a45a6c2562b086b8ae288f7f13e
sva39 <sva39@sfu.ca>
2022-02-16 16:08:33 -0800

    Added most fields and methods for Grid class. Modified setTank() and cellHit() in Cell class to fit with Tank's setTank(x,y) and cellHit(x,y) functions.

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -2,4 +2,34 @@ package com.company;
 
 public class Grid {
     //Grid
+    Cell[][] grid = new Cell[10][10];
+
+    public Grid() {
+    }
+
+    public void grid() {
+        //Not sure what this does
+    }
+
+    public boolean isCellTank(int x, int y) {
+        return grid[x][y].isTank();
+    }
+
+    public boolean isCellHit(int x, int y) {
+        return grid[x][y].isHit();
+    }
+
+    public void setTank(int x, int y) {
+        Cell cell = grid[x][y];
+        cell.setTank();
+    }
+
+    public void cellHit(int x, int y) {
+        Cell cell = grid[x][y];
+        cell.cellHit();
+    }
+
+    public Cell getCell(int x, int y) {
+        return grid[x][y];
+    }
 }
