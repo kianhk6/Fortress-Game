6782686259993e38c55cad7f79b87ea896421aaf
Kian <kianhk6@gmail.com>
2022-02-17 17:06:24 -0800

    added the field whichTank to cell for helping th UI array in gamelogic
    added getter and setters for this field

--- src/com/company/GameLogic.java
+++ src/com/company/GameLogic.java
@@ -1,6 +1,11 @@
 package com.company;
 
 public class GameLogic {
+    private Boolean isUserTurn;
+    private int FortressHealth;
+    private int numOfTanks;
+    private Boolean isUserTheWinner;
+    private Boolean isGameFinished;
 
     //Game Logic
 }
